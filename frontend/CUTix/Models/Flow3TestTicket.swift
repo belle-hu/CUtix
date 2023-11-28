bbee7863c2431da12137b55cfc189f1c50169c55
Rica Craig <rc673@cornell.edu>
2023-11-28 02:52:38 -0500

    Flow3TestTicket Re-Add

--- frontend/CUTix/Models/Flow3TestTicket.swift
+++ frontend/CUTix/Models/Flow3TestTicket.swift
@@ -2,7 +2,20 @@
 //  Flow3TestTicket.swift
 //  CUTix
 //
-//  Created by Rica Craig on 11/28/23.
+//  Created by Rica Craig on 11/27/23.
 //
 
 import Foundation
+
+struct Flow3TestTicket {
+    let id: String
+    let time: String
+    let location: String
+    let organization: String
+    let price: String
+    let imageUrl: String
+}
+
+let dummyData: [Flow3TestTicket] = [
+    Flow3TestTicket(id: "id", time: "2023-11-05T07:30:00Z", location: "Ithaca", organization: "organization", price: "$20.00", imageUrl: "https://t4.ftcdn.net/jpg/02/23/36/01/360_F_223360125_GjNeYSEaIaJoCzSKVMcMhw1LV9A2HpuO.webp")
+]
