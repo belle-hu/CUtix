88b44c592be84379d023246db32ff503fb6d2097
Vin Bui <vdb23@cornell.edu>
2023-11-30 00:10:17 -0500

    Fixed issues

---
+++ frontend/CUTix/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  CUTix
+//
+//  Created by Maisie Yan on 11/28/23.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let name: String
+    let holdingTickets: [Ticket]
+    let netId: String
+}
